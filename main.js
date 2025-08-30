0aed4c224b804199f54a97c35888ea9c5ec9ee55
Salman <muhammad.s.shahid@outlook.com>
2025-08-30 09:41:29 -0400

    Initial files for project

---
+++ main.js
@@ -0,0 +1,9 @@
+// This is a simple check to make sure your JavaScript file is linked correctly.
+// You can see this message in your browser's developer console.
+console.log("main.js loaded!");
+
+// Step 2 will start here: Set up the Three.js scene, camera, and renderer.
+
+// Step 3 will add the Cannon-es physics world.
+
+// Step 4 will add the dice models and roll logic.
